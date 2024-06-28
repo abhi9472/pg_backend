5fd05a5f087e266af244b08af834f3332a0c63fe
Abhishek <abhishekkumarcse2015@gmail.com>
2024-06-29 02:36:23 +0530

    Controllers and routes configuration

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
 import express from "express"
 import cors from "cors"
-
 import cookieParser from "cookie-parser"
 
-app.use(cors({
-    origin:process.env.CORS_ORG,
-    credentials:true
+const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-const app=express()
 
+// const app=express()
+import userRouter from './routes/user.routes.js'
+// import { registerUser } from "./controllers/user.controller.js"
+
+app.use("/api/v1/users",userRouter)
 export {app}
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,8 @@
-import { Promise } from "mongoose"
-
-const asyncHandler =(requestHandler)=> (req,res,next)=>{
-    Promise.resolve(requestHandler(req,res,next)).catch((err)=>(err))
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
 }
 
-export {asyncHandler}
 
+export { asyncHandler }
